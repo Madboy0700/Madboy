83b4a9580bc98e749e4ce1ba0865ee7a69aeb1e5
BaymoI <adamsnrobin@gmail.com>
2024-07-16 13:02:08 +0300

    Update bot.py

--- ArchMusic/core/bot.py
+++ ArchMusic/core/bot.py
@@ -1,5 +1,6 @@
 import sys
-
+import asyncio
+from datetime import timedelta
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeAllGroupChats
@@ -44,13 +45,14 @@ async def set_commands(client):
 
 class ArchMusic(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Bot Başlatılıyor")
+        LOGGER(__name__).info("Bot Başlatılıyor")
         super().__init__(
             "ArchMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
         )
+        self.restart_interval = timedelta(hours=4)  # Varsayılan olarak 4 saat
 
     async def start(self):
         await super().start()
@@ -68,14 +70,13 @@ class ArchMusic(Client):
                     video=video_url,
                     caption=caption,
                 )
-            except:
+            except Exception as e:
                 LOGGER(__name__).error(
                     "Bot log grubuna erişemedi. Log kanalınıza botunuzu eklediğinizden ve yönetici olarak terfi ettirdiğinizden emin olun!"
                 )
                 sys.exit()
 
             await set_commands(self)  
-
 
             a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
             if a.status != ChatMemberStatus.ADMINISTRATOR:
@@ -89,8 +90,26 @@ class ArchMusic(Client):
             sys.exit()
 
         if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
+            self.name = f"{get_me.first_name} {get_me.last_name}"
         else:
             self.name = get_me.first_name
 
         LOGGER(__name__).info(f"MusicBot {self.name} olarak başlatıldı")
+
+        
+        self.schedule_restart() # Res Fonksiyon Başla
+
+    async def restart_bot(self):
+        LOGGER(__name__).info("Bot yeniden başlatılıyor...")
+        await self.stop()
+        await self.start()
+
+    def schedule_restart(self):
+        loop = asyncio.get_event_loop()
+        
+        async def restart_at_intervals():
+            while True:
+                await asyncio.sleep(self.restart_interval.total_seconds())
+                await self.restart_bot()
+
+        loop.create_task(restart_at_intervals())
